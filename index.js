5ce7b6efa0f5a8a1dd7ee3750255b7d3b3d5e492
m-sohaibnadeem <sohaib@idenfo.com>
2024-09-03 14:11:24 +0500

    Fixed export

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useWindowSize = exports.useSpeechSynthesis = exports.useNotifications = exports.useNetwork = exports.useMediaQuery = exports.useMediaDevices = exports.useLocalStorage = exports.useIntersectionObserver = exports.useIdle = exports.useGeolocation = exports.useFullscreen = exports.useDarkMode = exports.useClipboard = exports.useBattery = void 0;
+var src_1 = require("./src/");
+Object.defineProperty(exports, "useBattery", { enumerable: true, get: function () { return src_1.useBattery; } });
+Object.defineProperty(exports, "useClipboard", { enumerable: true, get: function () { return src_1.useClipboard; } });
+Object.defineProperty(exports, "useDarkMode", { enumerable: true, get: function () { return src_1.useDarkMode; } });
+Object.defineProperty(exports, "useFullscreen", { enumerable: true, get: function () { return src_1.useFullscreen; } });
+Object.defineProperty(exports, "useGeolocation", { enumerable: true, get: function () { return src_1.useGeolocation; } });
+Object.defineProperty(exports, "useIdle", { enumerable: true, get: function () { return src_1.useIdle; } });
+Object.defineProperty(exports, "useIntersectionObserver", { enumerable: true, get: function () { return src_1.useIntersectionObserver; } });
+Object.defineProperty(exports, "useLocalStorage", { enumerable: true, get: function () { return src_1.useLocalStorage; } });
+Object.defineProperty(exports, "useMediaDevices", { enumerable: true, get: function () { return src_1.useMediaDevices; } });
+Object.defineProperty(exports, "useMediaQuery", { enumerable: true, get: function () { return src_1.useMediaQuery; } });
+Object.defineProperty(exports, "useNetwork", { enumerable: true, get: function () { return src_1.useNetwork; } });
+Object.defineProperty(exports, "useNotifications", { enumerable: true, get: function () { return src_1.useNotifications; } });
+Object.defineProperty(exports, "useSpeechSynthesis", { enumerable: true, get: function () { return src_1.useSpeechSynthesis; } });
+Object.defineProperty(exports, "useWindowSize", { enumerable: true, get: function () { return src_1.useWindowSize; } });
---
+++ index.ts
@@ -0,0 +1,13 @@
+export {
+    useBattery,
+    useClipboard,
+    useDarkMode, 
+    useFullscreen, 
+    useGeolocation, 
+    useIdle, 
+    useIntersectionObserver,
+     useLocalStorage,
+      useMediaDevices, 
+     useMediaQuery, 
+     useNetwork, useNotifications, useSpeechSynthesis, useWindowSize
+} from "./src/"
